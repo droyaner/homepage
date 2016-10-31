2650ae5b184a56bac252f28bb912b898f3650c23
Droyaner <Droyaner@gmx.de>
2016-10-31 20:49:48 +0100

    new email Eltern

--- html/buchung.php
+++ html/buchung.php
@@ -42,11 +42,10 @@ if (isset($_POST["submit"]) || isset($_POST["reset"])) {
    //Daten auswerten
    /******* FOR TEST *******/
    //$empfaenger = "Droyaner@gmx.de"; //Mailadresse
-   //$empfaenger = "marcus.treu@fau.de"; //Mailadresse
    /******* FOR BETA *******/
-   $empfaenger = "Droyaner@gmx.de,werner.treu@t-online.de";
+   $empfaenger = "Droyaner@gmx.de,werner.treu@gmx.net";
    /******* FOR Final *******/
-   //$empfaenger = "werner.treu@t-online.de"; 
+   //$empfaenger = "werner.treu@gmx.net"; 
    
    
    $betreff    = "Buchungsanfrage: $uhrzeit, $datum";

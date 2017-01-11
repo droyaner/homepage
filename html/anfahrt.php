808d5520807bf71332600cf7a44e3857cc444b26
Marcus Treu <marcus.treu@fau.de>
2017-01-11 16:13:06 +0100

    fix regioHeldenMap

--- html/anfahrt.php
+++ html/anfahrt.php
@@ -1,7 +1,7 @@
 
 <div id="content">
 	<h1>Anfahrt per Auto</h1>
-		<div align="center" style="width: 700px"><iframe width="700" height="400" src="http://regiohelden.de/google-maps/map.php?width=700&amp;height=400&amp;hl=de&amp;q=Am%20M%C3%BChlberg%2011%2C%2091788%20Pappenheim+(Ferienwohnung%20Treu)&amp;ie=UTF8&amp;t=p&amp;z=13&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a style="font-size: 9px;" href="http://www.regiohelden.de/google-maps/" style="font-size: 9px;">Google Maps Generator</a> by <a href="http://www.regiohelden.de/">RegioHelden</a></iframe><br /><span style="font-size: 9px;"><a style="font-size: 9px;" href="http://www.regiohelden.de/google-maps/" style="font-size: 9px;">Google Maps Generator</a> by <a href="http://www.regiohelden.de/" style="font-size: 9px;">RegioHelden</a></span></div>
+	<div style="width: 700px"><iframe width="700" height="400" src="https://www.regiohelden.de/google-maps/map.php?width=700&amp;height=400&amp;hl=de&amp;q=Am%20M%C3%BChlberg%2011%2C%2091788%20Pappenheim+(Ferienwohnung%20Treu)&amp;ie=UTF8&amp;t=p&amp;z=14&amp;iwloc=A&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a style="font-size: 9px;" href="https://www.regiohelden.de/google-maps/" style="font-size: 9px;">Google Maps Generator</a> by <a href="https://www.regiohelden.de/">RegioHelden</a></iframe><br /><span style="font-size: 9px;"><a style="font-size: 9px;" href="https://www.regiohelden.de/google-maps/" style="font-size: 9px;">Google Maps Generator</a> by <a href="https://www.regiohelden.de/" style="font-size: 9px;">RegioHelden</a></span></div>	
 	<p>
 		<b>Tipp:</b>
 		Beim Anklicken der roten Markierung öffnet sich der Routenplaner und Sie können Ihre Anreise nach Pappenheim planen.

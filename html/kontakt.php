1eace2eca2186bab702578ab8b719a9a2ed70177
Marcus Treu <marcus.treu@mail.de>
2018-05-13 18:56:20 +0200

    Impressum Remake

--- html/kontakt.php
+++ html/kontakt.php
@@ -1,73 +1,63 @@
 <div id="content">
-	<h1>Postanschrift</h1>
-	<p>
-		Werner und Christine Treu
-		<br />
-		Am Mühlberg 11
-		<br />
-		91788 Pappenheim
-		<br />
-		<br />
-		<b>E-Mail:&nbsp;&nbsp;</b> 
-		<a href="mailto:werner.treu@gmx.net">werner.treu@gmx.net</a><br />
-		<b>Telefon:</b> 09143/1795 <br />
-		<b>Handy:</b> &nbsp;&nbsp;0162/7461088 <br />
-		<br />
-		Fragen Sie einfach nach ihrem Wunschtermin für ihren Urlaub. 
-		<br />
-		Gerne senden wir Ihnen auch unseren Flyer zu.  
-		<br />
-		Wir würden uns freuen Sie als unsere Gäste begrüßen zu dürfen.
-		<br />
-		<a href="download/pdf_server.php?file=Flyer.pdf"><b>Download Flyer.pdf (0.4 MB)</b></a>
-		<br />
-		<br />
-		<br />
-	</p>
 
-	<h1>Impressum</h1>
-	<p>
+<h1>Impressum</h1> 
 
-		Angaben gemäß § 5 TMG
-		<br />
-		<br />
-		Werner u. Christine Treu 
-		<br />
-		Am Mühlberg 11
-		<br />
-		91788 Pappenheim
-		<br />
-		<br />
-	</p>
+<h2>Angaben gem&auml;&szlig; &sect; 5 TMG:</h2> 
+<p>Werner und Christine Treu<br/>
+Am Mühlberg 11 <br/> 
+91788 Pappenheim</p> 
 
-	<h2>Haftungsausschluss: </h2>
-	<p>
-		<b>Haftung für Inhalte</b>
-		<br />
-		Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
-		<br />
-		<br />
-		<b>Haftung für Links</b>
-		<br />
-		Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
-		<br />
-		<br />
-		<b>Urheberrecht</b>
-		<br />
-		Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
-		<br />
-		<br />
-		<b>Datenschutz</b>
-		<br />
-		Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. 
-		<br />
-		Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. 
+<h2>Kontakt:</h2>
+<p>Telefon: +49 (0) 9143 1795<br/> 
+E-Mail: werner.treu@gmx.net<br/>
+<a href="download/pdf_server.php?file=Flyer.pdf"><b>Download Flyer.pdf (0.4 MB)</b></a>
+</p>
 
-		<br />
-		Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
-		<br />
-		<br />
-		Quelle: 
-		<a href="http://www.impressum-generator.de/" title="Generator">Impressum Generator</a>
-	</p>
+
+<h2>Haftung f&uuml;r Inhalte</h2> 
+<p>
+	Als Diensteanbieter sind wir
+	gem&auml;&szlig; &sect; 7 Abs.1 TMG f&uuml;r eigene Inhalte auf diesen Seiten nach den allgemeinen
+	Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+	verpflichtet, &uuml;bermittelte oder gespeicherte fremde Informationen zu &uuml;berwachen oder nach
+	Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit hinweisen.</p>
+<p>
+	Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen
+	Gesetzen bleiben hiervon unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab dem
+	Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung m&ouml;glich. Bei Bekanntwerden von
+	entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+</p> 
+
+<p>
+Diese Webseite verwendet das Produkt Google Maps von Google Inc. Durch Nutzung dieser Webseite erklären Sie sich mit der Erfassung, Bearbeitung sowie Nutzung der automatisiert erhobenen Daten durch Google Inc, deren Vertreter sowie Dritter einverstanden. 
+Die Nutzungsbedingungen von Google Maps finden sie unter 
+<a href="http://www.google.com/intl/de_de/help/terms_maps.html">Nutzungsbedingungen von Google Maps</a>.
+</p> 
+
+<h2>Haftung f&uuml;r Links</h2> 
+<p>
+	Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf deren Inhalte
+	wir keinen Einfluss haben. Deshalb k&ouml;nnen wir f&uuml;r diese fremden Inhalte auch keine
+	Gew&auml;hr &uuml;bernehmen. F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter
+	oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf
+	m&ouml;gliche Rechtsverst&ouml;&szlig;e &uuml;berpr&uuml;ft. Rechtswidrige Inhalte waren zum
+	Zeitpunkt der Verlinkung nicht erkennbar.
+</p> 
+<p>
+	Eine permanente inhaltliche Kontrolle der verlinkten
+	Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+	Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+</p>
+<h2>Urheberrecht</h2> 
+<p>
+	Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten
+	unterliegen dem deutschen Urheberrecht. Die Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede
+	Art der Verwertung au&szlig;erhalb der Grenzen des Urheberrechtes bed&uuml;rfen der schriftlichen
+	Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur f&uuml;r
+	den privaten, nicht kommerziellen Gebrauch gestattet.</p> <p>Soweit die Inhalte auf dieser Seite nicht
+	vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
+	Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam
+	werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen 
+	werden wir derartige Inhalte umgehend entfernen.</p> <p>Quelle: <a href="https://www.e-recht24.de">erecht24.de</a>
+</p>
 </div>
